e0c088b2ccd2e5c8f9367b1deaedf2f6df627713
tuicaodan <diaoyiwei@gmail.com>
2021-08-13 13:50:05 +1200

    finished github oauth: user can login through api and will return the user data and jwt token

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import styled from "styled-components";
 import tw from "twin.macro";
 import HomePage from "./app/containers/HomePage";
+import AuthContext from "./context/auth-context";
 
 const AppContainer = styled.div`
   ${tw`
@@ -16,10 +17,11 @@ const AppContainer = styled.div`
 function App() {
   return (
     <AppContainer>
-      <HomePage />
+      <AuthContext.Provider>
+        <HomePage />
+      </AuthContext.Provider>
     </AppContainer>
   );
 }
-
 
 export default App;
--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 import tw from "twin.macro";
 import { slide as Menu } from "react-burger-menu";
 import { useMediaQuery } from "react-responsive";
 import { SCREENS } from "../responsive";
-import menuStyles from "./menuStyles"
+import menuStyles from "./menuStyles";
 
 const ListContainer = styled.ul`
   ${tw`
@@ -13,7 +14,7 @@ const ListContainer = styled.ul`
 `}
 `;
 
-const NavItem = styled.li<{menu?: any}>`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
     text-xs
     md:text-base
@@ -26,18 +27,63 @@ const NavItem = styled.li<{menu?: any}>`
     ease-in-out
     hover:text-gray-700
 `}
-${({menu}) => menu&& css`
-  ${tw`
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
     text-white
     text-xl
     mb-3
     focus:text-white
   `}
-`}
+    `}
 `;
 
 const NavItems = () => {
-  const isMobile = useMediaQuery({ maxWidth:SCREENS.sm });
+  const CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+
+  //const [data, setData] = useState({ errorMessage: "", isLoading: false });
+
+  useEffect(() => {
+    const loginMethod = async () => {
+      const params = new URLSearchParams(window.location.search);
+      let code = params.get("code");
+      if (code != null) {
+        try {
+          const requestBody = {
+            query: `
+              mutation {
+                login(access_code: "${code}")
+              }
+            `,
+          };
+          fetch("http://localhost:4000/graphql", {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((res) => {
+              if (res.status !== 200 && res.status !== 201) {
+                throw new Error("Login Failed");
+              }
+              return res.json();
+            })
+            .then((resData) => {
+              const loginedUser = JSON.parse(resData.data["login"]);  
+              console.log(loginedUser);
+            });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+    loginMethod();
+  }, []);
+
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
 
   if (isMobile)
     return (
@@ -52,13 +98,18 @@ const NavItems = () => {
       </Menu>
     );
 
-
   return (
     <ListContainer>
       <NavItem>
         <a href="./">Home</a>
       </NavItem>
-      <NavItem>Login</NavItem>
+      <NavItem>
+        <a
+          href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+        >
+          Login
+        </a>
+      </NavItem>
       <NavItem>About</NavItem>
     </ListContainer>
   );
